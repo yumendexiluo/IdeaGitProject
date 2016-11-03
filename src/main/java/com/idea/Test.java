93895d587e5a22c4a0245634e5e3e48f09cd0c97
yangshuxi <122294322@qq.com>
2016-11-03 11:36:33 +0800

    hello world

---
+++ src/main/java/com/idea/Test.java
@@ -0,0 +1,10 @@
+package com.idea;
+
+/**
+ * Created by admin on 2016/11/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
